ea31a6a38229a49c80a1bb232093eb41c4e45081
Cillié Malan <cilliemalan@gmail.com>
2018-02-14 16:45:09 +0200

    patching in webhooks

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+
+const webhooks = require('./webhooks');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+
 app.use(express.static('public'));
+app.use(/\/webhooks\//, webhooks());
 app.listen(port, () => console.log(`Listening on port ${port}!`));
