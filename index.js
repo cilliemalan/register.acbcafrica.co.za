554bba95d866e88b2fc866859525bd04db74bf7f
Cillié Malan <cilliemalan@gmail.com>
2018-02-14 21:30:19 +0200

    Added some logging initialization

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const express = require('express');
+const winston = require('winston');
 
 const webhooks = require('./webhooks');
 
 const app = express();
 
+// env
 const port = process.env.PORT || 3000;
-const secret = process.env.HOOK_SECRET;
+const ghsecret = process.env.GITHUB_HOOK_SECRET;
+const loglevel = process.env.LOGLEVEL || 'verbose';
+
+// set up logging
+winston.configure({
+    transports: [new (winston.transports.Console)({ level: loglevel, colorize: true })]
+});
+winston.info('starting...');
+winston.info(`loglevel: ${loglevel}`);
+winston.info('port: %s', port);
+winston.info('github hook secret: %s', !!ghsecret);
 
 
+
+// set up pipeline
 app.use(express.static('public'));
-app.use('/webhooks', webhooks(secret));
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+app.use('/webhooks', webhooks(ghsecret));
+app.listen(port, () => winston.info(`Listening on port ${port}!`));
