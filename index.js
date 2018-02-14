c5540e9ffd73a02cdd113d89601654ad86f6df6c
Cillié Malan <cilliemalan@gmail.com>
2018-02-14 21:03:15 +0200

    hook secret from env

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const webhooks = require('./webhooks');
 const app = express();
 
 const port = process.env.PORT || 3000;
+const secret = process.env.HOOK_SECRET;
 
 
 app.use(express.static('public'));
-app.use(/\/webhooks\//, webhooks());
+app.use(/\/webhooks\//, webhooks(secret));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
