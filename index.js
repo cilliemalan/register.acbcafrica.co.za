5124197c9de52bf19b1034342dfa40786c2d0f2a
Cillié Malan <cilliemalan@gmail.com>
2018-02-14 21:06:15 +0200

    Fixed webhook path

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ const secret = process.env.HOOK_SECRET;
 
 
 app.use(express.static('public'));
-app.use(/\/webhooks\//, webhooks(secret));
+app.use('/webhooks', webhooks(secret));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
