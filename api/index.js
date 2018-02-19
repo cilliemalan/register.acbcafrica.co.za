f5ee1f36b3ed6c250268443645ef217feeb2961a
Cillié Malan <cilliemalan@gmail.com>
2018-02-19 14:08:38 +0200

    Added authentication for API

--- api/index.js
+++ api/index.js
@@ -1,13 +1,33 @@
 const winston = require('winston');
 const express = require('express');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
 
+const jwtIssuer = process.env.JWT_ISSUER || "https://retro.eu.auth0.com/";
+const jwtAudience = process.env.JWT_AUDIENCE || 'https://malan.wedding/api';
 
 const api = express.Router();
+
+var jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `${jwtIssuer}.well-known/jwks.json`
+    }),
+    credentialsRequired: true,
+    audience: jwtAudience,
+    issuer: jwtIssuer,
+    algorithms: ['RS256']
+});
+
+
+api.use(jwtCheck);
 
 api.use((req, res, next) => {
     try {
         next();
-        winston.verbose('API %s request for %s by %s -> ', req.method, req.url, req.user || '(anonymous)', res.statusCode);
+        winston.verbose('API %s request for %s by %j -> ', req.method, req.url, (req.user && req.user.sub) || 'anonymous', res.statusCode);
     } catch (e) {
         winston.error('Exception during request: %j', e);
         throw e;
