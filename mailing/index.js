56e665b8f6931dff1a2a4a4fa7bac4c95cdc39b6
Cillié Malan <cilliemalan@gmail.com>
2018-02-20 14:24:21 +0200

    Removed mailing stuff

--- mailing/index.js
+++
@@ -1,9 +0,0 @@
-
-const mail = require('./mail_jobs');
-
-
-module.exports = {
-    sendEmail: function(from, to, subject, body, attachments) {
-        mail.sendEmail(from, to, subject, body, attachments);
-    }
-}
