6661b9c08fbca981e5276bd2f037daa63a7dd4eb
Cillié Malan <cilliemalan@gmail.com>
2018-02-21 23:25:39 +0200

    Login display on header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import logoUrl from '../content/logo.png'
 import { Link } from 'react-router-dom'
+import { user } from '../auth';
 
-export const Header = (props) =>
-    <nav>
+export const Header = (props) => {
+    const loggedIn = !!user;
+    return <nav>
         <Link to="/" className="navlogo">
             <img src={logoUrl} alt="ACBC Africa Registration Home" title="ACBC Africa Registration Home" />
             <span>ACBC Africa Registration</span>
         </Link>
-    </nav>;
+        <div class="profile">
+            {!loggedIn
+                ? <div>
+                    Not Signed in.&nbsp;<a href="/auth/login">Sign in</a>&nbsp;or&nbsp;<a href="/auth/login">register</a>.
+                </div>
+                : <div>
+                    <span>Signed in as {user.name}<br /> <a href="/auth/logout">Sign out</a></span>
+                    <img src={user.picture} alt={user.name} />
+                </div>
+            }
+        </div>
+    </nav>
+};
