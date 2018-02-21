6238f473834f036ad305485909bfabb363ad1bcb
Cillié Malan <cilliemalan@gmail.com>
2018-02-21 20:53:09 +0200

    api using config

--- api/index.js
+++ api/index.js
@@ -3,49 +3,48 @@ const express = require('express');
 const jwt = require('express-jwt');
 const jwks = require('jwks-rsa');
 
-const jwtIssuer = process.env.JWT_ISSUER || "https://malan-wedding.eu.auth0.com/";
-const jwtAudience = process.env.JWT_AUDIENCE || 'https://malan.wedding/api';
+const config = require('../config');
 
-const api = express.Router();
+module.exports = () => {
 
-var jwtCheck = jwt({
-    secret: jwks.expressJwtSecret({
-        cache: true,
-        rateLimit: true,
-        jwksRequestsPerMinute: 5,
-        jwksUri: `${jwtIssuer}.well-known/jwks.json`
-    }),
-    credentialsRequired: true,
-    audience: jwtAudience,
-    issuer: jwtIssuer,
-    algorithms: ['RS256']
-});
-
-
-
-api.use((req, res, next) => {
-    res.on('finish', () => {
-        winston.verbose('API %s request for %s by %j -> %s', req.method, req.url, (req.user && req.user.sub) || 'anonymous', res.statusCode);
+    const api = express.Router();
+    
+    var jwtCheck = jwt({
+        secret: jwks.expressJwtSecret({
+            cache: true,
+            rateLimit: true,
+            jwksRequestsPerMinute: 5,
+            jwksUri: `${config.issuer}.well-known/jwks.json`
+        }),
+        credentialsRequired: true,
+        audience: config.audience,
+        issuer: config.issuer,
+        algorithms: ['RS256']
     });
-    next();
-});
+    
+    
+    
+    api.use((req, res, next) => {
+        res.on('finish', () => {
+            winston.verbose('API %s request for %s by %j -> %s', req.method, req.url, (req.user && req.user.sub) || 'anonymous', res.statusCode);
+        });
+        next();
+    });
+    
+    api.use(jwtCheck);
+    
+    api.get('/', (req, res) => res.json({ status: 'ok' }));
+    
+    
+    api.use((err, req, res, next) => {
+        if (err.name == "UnauthorizedError") {
+            winston.verbose('received auth error %j', err);
+            res.status(403).json({ error: 'authorization', message: err.message, code: err.code });
+        } else {
+            winston.warn('request error for %s request for %s by %j -> %j', req.method, req.url, (req.user && req.user.sub) || 'anonymous', err);
+            res.status(500).json({ error: 'server', message: 'An internal server error occurred.' });
+        }
+    });
 
-api.use(jwtCheck);
-
-api.get('/', (req, res) => res.json({ status: 'ok' }));
-
-
-
-
-
-
-api.use((err, req, res, next) => {
-    if (err.name == "UnauthorizedError") {
-        winston.verbose('received auth error %j', err);
-        res.status(403).json({ error: 'authorization', message: err.message, code: err.code });
-    } else {
-        winston.warn('request error for %s request for %s by %j -> %j', req.method, req.url, (req.user && req.user.sub) || 'anonymous', err);
-        res.status(500).json({ error: 'server', message: 'An internal server error occurred.' });
-    }
-})
-module.exports = api;
+    return api;
+};
