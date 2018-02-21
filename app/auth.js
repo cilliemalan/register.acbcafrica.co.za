f05e5411433e3a0dbaa89f6c069a805d1c539583
Cillié Malan <cilliemalan@gmail.com>
2018-02-21 22:45:37 +0200

    Auth clientside working now

--- app/auth.js
+++ app/auth.js
@@ -1,3 +1,15 @@
 
+function getCookie(name) {
+    const match = document.cookie.match(new RegExp('(?:^| )' + name + '=([^;]+)'));
+    if (match) {
+        const decoded = decodeURIComponent(match[1]);
+        try {
+            return JSON.parse(decoded);
+        } catch (e) {
+            console.error(e);
+            return null;
+        }
+    }
+}
 
-export const user = null;
+export const user = getCookie('user');
