df917a2b171162d400df9be9635db7329a2d169d
Cillié Malan <cilliemalan@gmail.com>
2018-02-24 17:54:27 +0200

    Removeed profile stuffs

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,16 +10,5 @@ export const Header = (props) => {
             <img src={logoUrl} alt="ACBC Africa Registration Home" title="ACBC Africa Registration Home" />
             <span>ACBC Africa Registration</span>
         </Link>
-        <div className="profile">
-            {!loggedIn
-                ? <div>
-                    Not Signed in.&nbsp;<a href="/auth/login">Sign in</a>&nbsp;or&nbsp;<a href="/auth/login">register</a>.
-                </div>
-                : <div>
-                    <span>Signed in as {user.name}<br /> <a href="/auth/logout">Sign out</a></span>
-                    <img src={user.picture} alt={user.name} />
-                </div>
-            }
-        </div>
     </nav>
 };
