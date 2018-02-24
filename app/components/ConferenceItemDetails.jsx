7fd092335d20e6f486a760986e3a4818982183a4
Cillié Malan <cilliemalan@gmail.com>
2018-02-24 16:42:02 +0200

    Date and currency formatting

--- app/components/ConferenceItemDetails.jsx
+++ app/components/ConferenceItemDetails.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import moment from 'moment';
 
 export class ConferenceItemDetails extends React.Component {
     constructor(props) {
@@ -49,8 +49,33 @@ export class ConferenceItemDetails extends React.Component {
     }
 
     render() {
-        const formatDate = (a) => "fmt";
-        const formatCost = (a) => "cst";
+        const formatSingleDate = (date) => {
+            const dateOnly = date.getHours() == 0 &&
+                date.getMinutes() == 0 &&
+                date.getSeconds() == 0 &&
+                date.getMilliseconds() == 0;
+
+            return dateOnly ? moment(date).format('dddd D MMMM')
+                : moment(date).format('ddd D MMM HH:mm');
+        };
+        const formatDate = (from, to) => {
+            return to ? `${formatSingleDate(from)} — ${formatSingleDate(to)}`
+                : `${formatSingleDate(from)}`;
+        };
+        const formatCost = (a) => {
+            if (typeof a == 'number') {
+                if (a == 0) {
+                    return 'Free';
+                } else {
+                    return a.toLocaleString('en-ZA', {
+                        style: 'currency',
+                        currency: 'ZAR',
+                    });
+                }
+            } else {
+                return '';
+            }
+        }
         return <div>
             <h2>Conference Options</h2>
             <p>Please select all that apply.</p>
