45ea1f5794a22cc4190e505a3f69795c81139cfb
Cillié Malan <cilliemalan@gmail.com>
2018-02-26 23:12:38 +0200

    Some reducers

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,4 +1,30 @@
+import { combineReducers } from 'react';
 
-export default (state, action) => {
-    return state;
+function forms(state = { items: {} }, action) {
+    switch (action.type) {
+        case 'FETCHING_FORMS':
+            return { ...state, loading: true };
+        case 'FETCHED_FORMS':
+            return {
+                ...state,
+                items: action.items,
+                loading: false
+            };
+        case 'ADD_ENTRY':
+            const { form, entry } = action;
+            return {
+                ...state,
+                items: {
+                    ...state.items,
+                    [form]: {
+                        ...state.items[form],
+                        entries: [...state.items[form], entry]
+                    }
+                }
+            };
+        default:
+            return state;
+    }
 }
+
+export default combineReducers({ forms });
