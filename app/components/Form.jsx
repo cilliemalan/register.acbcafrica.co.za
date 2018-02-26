890f05bef1e14809a21f198ff6ab0f0771987848
Cillié Malan <cilliemalan@gmail.com>
2018-02-26 23:19:23 +0200

    Functional Form

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -9,32 +9,14 @@ import facebook_logo from '../content/facebook-logo.png';
 import google_logo from '../content/google-logo.png';
 import padlock_image from '../content/padlock.png';
 
-export class Form extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-
-    componentWillMount() {
-        const props = this.props;
-        const thisFormName = props.match.params.id;
-        this.setState({ form: forms[thisFormName] });
-    }
-
-    render() {
-        const form = this.state.form;
-        if (!form) {
-            return <div className="form"></div>
-        } else {
-            return <div className="form">
-                <img className="headerImage" src={form.image} alt={form.title} />
-                <Link to="/">&lt;&lt;&nbsp;Go&nbsp;Back</Link>
-                <h1>{form.title}</h1>
-                <p>You are registering for <strong>{form.title}</strong>. Please fill in the details below.</p>
-                <form>
-                    <PersonalDetails />
-                    <ConferenceItemDetails options={form.options} />
-                </form>
-            </div>;
-        }
-    }
-}
+export const Form = ({ form }) =>
+    <div className="form">
+        <img className="headerImage" src={form.image} alt={form.title} />
+        <Link to="/">&lt;&lt;&nbsp;Go&nbsp;Back</Link>
+        <h1>{form.title}</h1>
+        <p>You are registering for <strong>{form.title}</strong>. Please fill in the details below.</p>
+        <form>
+            <PersonalDetails />
+            <ConferenceItemDetails options={form.options} />
+        </form>
+    </div>;
