3e0e5863f636cd34cb5e32ded8099844b4d4d202
Cillié Malan <cilliemalan@gmail.com>
2018-02-27 00:00:30 +0200

    Connected App

--- app/App.jsx
+++ app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Route, Link, BrowserRouter, Switch } from 'react-router-dom'
+import { connect } from 'react-redux';
 import { Home } from './components/Home';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
@@ -8,19 +9,25 @@ import { PrivacyPolicy } from './components/PrivacyPolicy';
 import { NotFound } from './components/NotFound';
 import { Form } from './components/Form';
 
-export const App = () =>
+const mapStateToProps = state => ({ forms: state.forms.items });
+
+let App = ({ forms }) =>
     <BrowserRouter>
         <div>
             <Header />
             <div id="mainContent">
                 <Switch>
-                    <Route exact path="/" render={Home} />
-                    <Route exact path="/terms-and-conditions" render={TermsAndConditions} />
-                    <Route exact path="/privacy-policy" render={PrivacyPolicy} />
-                    <Route path="/register/:id" component={Form} />
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/terms-and-conditions" component={TermsAndConditions} />
+                    <Route exact path="/privacy-policy" component={PrivacyPolicy} />
+                    <Route path="/register/:id" render={({id}) => <Form form={forms[id]} />} />
                     <Route component={NotFound} />
                 </Switch>
             </div>
             <Footer />
         </div>
     </BrowserRouter>;
+
+App = connect(mapStateToProps)(App);
+
+export { App };
