5850985aac60eee4de080a2b46db07f0e754e23b
Cillié Malan <cilliemalan@gmail.com>
2018-02-27 20:02:41 +0200

    Trying (in vain) to load forms from json

--- app/containers/Home.jsx
+++ app/containers/Home.jsx
@@ -1,45 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { fetchForms } from '../actions';
 const Fragment = React.Fragment;
-
-
 
 const mapStateToProps = (state) => ({
     forms: state.forms.items,
     loading: state.forms.loading
 });
 
-let Home = ({ forms, loading }) => {
+class Home extends React.Component {
 
-    const sortedForms = Object.keys(forms);
-    const any = !!sortedForms.length;
-    sortedForms.sort((a, b) => forms[a].from > forms[b].from);
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(fetchForms());
+    }
 
-    const loadingMessage = () => "Loading...";
-    const nothingMessage = () => "Nothing is available at this time. Please check again later!";
-    const formListItems = () => <Fragment>
-        <p>What would you like to register for?</p>
-        <ul className="formSelection">
-            {
-                sortedForms.map(k =>
-                    <li key={k}>
-                        <Link to={`/register/${k}`}>
-                            <img src={forms[k].image} alt={forms[k].title} />
-                        </Link>
-                    </li>)
-            }
-        </ul>
-    </ Fragment>;
-    const formList = () => (any ? formListItems() : nothingMessage());
+    render() {
+        const { forms, loading } = this.props;
+        const sortedForms = Object.keys(forms);
+        const any = !!sortedForms.length;
+        sortedForms.sort((a, b) => forms[a].from > forms[b].from);
 
-    return <div>
-        <h1>ACBC Africa Registration</h1>
-        {loading
-            ? loadingMessage()
-            : formList()}
-    </div>
-};
+        const loadingMessage = () => "Loading...";
+        const nothingMessage = () => "Nothing is available at this time. Please check again later!";
+        const formListItems = () => <Fragment>
+            <p>What would you like to register for?</p>
+            <ul className="formSelection">
+                {
+                    sortedForms.map(k =>
+                        <li key={k}>
+                            <Link to={`/register/${k}`}>
+                                <img src={forms[k].image} alt={forms[k].title} />
+                            </Link>
+                        </li>)
+                }
+            </ul>
+        </ Fragment>;
+        const formList = () => (any ? formListItems() : nothingMessage());
+
+        return <div>
+            <h1>ACBC Africa Registration</h1>
+            {loading
+                ? loadingMessage()
+                : formList()}
+        </div>
+    }
+}
 
 Home = connect(mapStateToProps)(Home);
 
