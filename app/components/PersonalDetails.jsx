233c4949e896a615f533b7f7267d2162512584ff
Cillié Malan <cilliemalan@gmail.com>
2018-03-03 21:30:16 +0200

    Sorreeee sheesh

--- app/components/PersonalDetails.jsx
+++ app/components/PersonalDetails.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-
 export class PersonalDetails extends React.Component {
     constructor(props) {
         super(props);
@@ -23,27 +22,27 @@ export class PersonalDetails extends React.Component {
             <p>Please the personal information for the registrant.</p>
             <div className="form-group required">
                 <label htmlFor="title">Title</label>
-                <input type="text" id="title" autocomplete="honorific-prefix" name="title" />
+                <input type="text" id="title" autoComplete="honorific-prefix" name="title" />
             </div>
             <div className="form-group required">
                 <label htmlFor="firstname">First name</label>
-                <input type="text" id="firstname" autocomplete="given-name" name="firstname" />
+                <input type="text" id="firstname" autoComplete="given-name" name="firstname" />
             </div>
             <div className="form-group required">
                 <label htmlFor="lastname">Last name</label>
-                <input type="text" id="lastname" autocomplete="family-name" name="lastname" />
+                <input type="text" id="lastname" autoComplete="family-name" name="lastname" />
             </div>
             <div className="form-group required">
                 <label htmlFor="contactNumber">Contact Number</label>
-                <input type="text" id="contactNumber" autocomplete="tel" name="contactNumber" />
+                <input type="text" id="contactNumber" autoComplete="tel" name="contactNumber" />
             </div>
             <div className="form-group required">
                 <label htmlFor="email">Email address</label>
-                <input type="email" id="email" autocomplete="email" name="email" />
+                <input type="email" id="email" autoComplete="email" name="email" />
             </div>
             <div className="form-group required">
                 <label htmlFor="country">From which country are you?</label>
-                <input type="text" id="country" autocomplete="country-name" name="country" />
+                <input type="text" id="country" autoComplete="country-name" name="country" />
             </div>
             <div className="form-group">
                 <label htmlFor="church">From which church are you?</label>
