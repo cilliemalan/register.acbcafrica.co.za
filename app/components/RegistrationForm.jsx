c465adedc1006c57a162cc9b0a436ad195746a4c
Cillié Malan <cilliemalan@gmail.com>
2018-03-04 16:30:41 +0200

    Logging to console on submit for now

--- app/components/RegistrationForm.jsx
+++ app/components/RegistrationForm.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { RegistrationFormDetails } from './RegistrationFormDetails';
 
-export const RegistrationForm = ({ fillingForm }) =>
-    <div className="form">
+export const RegistrationForm = ({ fillingForm }) => {
+    const submit = (calues) => {
+        console.log(calues);
+    }
+
+    return <div className="form">
         <img className="headerImage" src={fillingForm.image} alt={fillingForm.title} />
         <Link to="/">&lt;&lt;&nbsp;Go&nbsp;Back</Link>
         <h1>{fillingForm.title}</h1>
         <p>You are registering for <strong>{fillingForm.title}</strong>. Please fill in the details below.</p>
-        <RegistrationFormDetails fillingForm={fillingForm} />
+        <RegistrationFormDetails fillingForm={fillingForm} onSubmit={submit} />
     </div>;
+}
