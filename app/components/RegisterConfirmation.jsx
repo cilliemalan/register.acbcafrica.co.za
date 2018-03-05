68ade67b27147da92de2be27e6b2c9d36c5c79c4
Cillié Malan <cilliemalan@gmail.com>
2018-03-05 22:26:08 +0200

    Added RegisterConfirmation component

---
+++ app/components/RegisterConfirmation.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { RegistrationForm } from './RegistrationForm';
+
+export const RegisterConfirmation = ({ details }) => {
+    
+}
