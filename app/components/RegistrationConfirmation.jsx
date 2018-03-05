86699be738ca5b4dde402984411fa0bbc18e153d
Cillié Malan <cilliemalan@gmail.com>
2018-03-05 23:58:23 +0200

    Fleshed out RegistrationConfirmation component

--- app/components/RegistrationConfirmation.jsx
+++ app/components/RegistrationConfirmation.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { RegistrationForm } from './RegistrationForm';
+import { SubmissionDetails } from './SubmissionDetails';
+import { PaymentDetails } from './PaymentDetails';
 
-export const RegistrationConfirmation = ({ submission }) => {
-    return <div>hello</div>;
+export const RegistrationConfirmation = ({ submission, forms }) => {
+    const formName = forms[submission.form].title;
+    const total = submission.details.options.total;
+    return <div>
+        <h1>Confirm Registration</h1>
+        <p>You are registering for <strong>{formName}</strong>.</p>
+        <SubmissionDetails submission={submission} />
+        { total > 0 && <PaymentDetails submission={submission} /> }
+    </div>;
 }
