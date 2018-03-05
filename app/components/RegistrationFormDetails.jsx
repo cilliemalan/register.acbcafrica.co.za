ff4931014cf45877d14026aae18230f21adc5fbe
Cillié Malan <cilliemalan@gmail.com>
2018-03-05 12:16:23 +0200

    Added validation for options

--- app/components/RegistrationFormDetails.jsx
+++ app/components/RegistrationFormDetails.jsx
@@ -5,14 +5,30 @@ import { reduxForm, Field } from 'redux-form'
 
 
 
-let RegistrationFormDetails = ({ fillingForm, handleSubmit }) =>
-    <form onSubmit={handleSubmit}>
+let RegistrationFormDetails = ({ fillingForm, handleSubmit }) => {
+
+    const validateConferenceOptions = (value = {}) => {
+        const requiredOptions = Object.keys(fillingForm.options)
+            .filter(key => fillingForm.options[key].required);
+        if (requiredOptions.length) {
+            const selectedRequiredOptions = requiredOptions.filter(key => value[key]);
+            if (!selectedRequiredOptions.length) {
+                const requiredOptionsTitles = requiredOptions.map(key => fillingForm.options[key].title)
+                    .join(", ");
+        
+                return `Please select at least one of: ${requiredOptionsTitles}`;
+            }
+        }
+    }
+
+    return <form onSubmit={handleSubmit}>
         <PersonalDetails />
-        <Field component={ConferenceItemDetails} name="options" options={fillingForm.options} />
+        <Field component={ConferenceItemDetails} name="options" validate={validateConferenceOptions} options={fillingForm.options} />
         <div className="form-group submit">
             <button>Submit</button>
         </div>
     </form>;
+}
 
 RegistrationFormDetails = reduxForm({
     form: 'personalDetails'
