84eb3a8e47eb08d6e29ef09e99ce7e57950b26d2
Cillié Malan <cilliemalan@gmail.com>
2018-03-05 23:34:32 +0200

    Register component passing up onSubmit event

--- app/components/Register.jsx
+++ app/components/Register.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { RegistrationForm } from './RegistrationForm';
 
-export const Register = ({ fillingForm }) => {
-    const submit = (values) => {
-        console.log(values);
-    }
+export const Register = ({ fillingForm, onSubmit }) => {
 
     return <div className="form">
         <img className="headerImage" src={fillingForm.image} alt={fillingForm.title} />
         <Link to="/">&lt;&lt;&nbsp;Go&nbsp;Back</Link>
         <h1>{fillingForm.title}</h1>
         <p>You are registering for <strong>{fillingForm.title}</strong>. Please fill in the details below.</p>
-        <RegistrationForm fillingForm={fillingForm} onSubmit={submit} />
+        <RegistrationForm fillingForm={fillingForm} onSubmit={onSubmit} />
     </div>;
 }
