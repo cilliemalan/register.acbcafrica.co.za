d76576c4559d4386aff634192d1aa36cbffefff8
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 10:49:58 +0200

    Twekaed payment details display

--- app/components/PaymentDetails.jsx
+++ app/components/PaymentDetails.jsx
@@ -5,18 +5,16 @@ import { formatCost } from '../helpers/formatting';
 export const PaymentDetails = ({ submission: { form, details: { options: { total }, lastname, firstname } } }) => {
     return <div>
         <h2>Payment</h2>
-        <p>Total Cost: <strong>{formatCost(total)}</strong></p>
-        <h3>Bank Account</h3>
         <p>
             Please direct payments to the SMTI Bank Account:<br />
             <br />
+            Payment Reference: <strong>{form}-{lastname} {firstname}</strong><br />
+            Amount: <strong>{formatCost(total)}</strong>< br/>
             Account Holder: <strong>Strengthening Ministries Training Institute</strong><br />
             Bank: <strong>Standard Bank</strong><br />
             Branch Code: <strong>051001</strong><br />
             Account Number: <strong>013350692</strong><br />
             Account Type: <strong>Current</strong><br />
-            <br />
-            Please specify as your payment reference: <strong>{form}-{lastname} {firstname}</strong><br />
             <br />
             Please Email proof of payment to: <a href="mailto:admin@acbcafrica.co.za">admin@acbcafrica.co.za</a>
         </p>
