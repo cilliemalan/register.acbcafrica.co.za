87fad99f8927184b1ef584cb789664202a175bf0
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 10:40:19 +0200

    Added options display instead of payment details on confirmation page.

--- app/components/RegistrationConfirmation.jsx
+++ app/components/RegistrationConfirmation.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { SubmissionDetails } from './SubmissionDetails';
 import { PaymentDetails } from './PaymentDetails';
+import { OptionsDisplay } from './OptionsDisplay';
 
 export const RegistrationConfirmation = ({ submission, forms }) => {
-    const formName = forms[submission.form].title;
+    const form = forms[submission.form];
+    const formName = form.title;
     const total = submission.details.options.total;
+    const options = form.options;
+    const selection = submission.details.options;
+
     return <div>
         <h1>Confirm Registration</h1>
         <p>You are registering for <strong>{formName}</strong>.</p>
         <SubmissionDetails submission={submission} />
-        { total > 0 && <PaymentDetails submission={submission} /> }
+        <OptionsDisplay options={options} selection={selection} />
     </div>;
 }
