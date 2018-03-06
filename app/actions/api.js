becb3d73cd5d788851199c097dee97ed941ee4f7
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 13:01:23 +0200

    fixed api call error handling

--- app/actions/api.js
+++ app/actions/api.js
@@ -1,11 +1,19 @@
 
+function handleError(e) {
+    if(typeof e == "string") {
+        throw e;
+    }
+
+    throw e.message || "an error has occurred.";
+}
+
 export const fetchForms = () => fetch('/data/forms.json')
     .then(response => response.json())
     .then(forms => {
         Object.values(forms).forEach(form => {
             form.from = form.from && new Date(form.from);
             form.to = form.to && new Date(form.to);
-
+        
             Object.values(form.options).forEach(option => {
                 option.from = option.from && new Date(option.from);
                 option.to = option.to && new Date(option.to);
@@ -13,7 +21,7 @@ export const fetchForms = () => fetch('/data/forms.json')
         });
 
         return forms;
-    });
+    }).catch(handleError);
 
 export const submitRegistration = (registration) => fetch('/api/submit', {
     method: 'POST',
@@ -22,5 +30,7 @@ export const submitRegistration = (registration) => fetch('/api/submit', {
         'Content-Type': 'application/json'
     }
 }).then(res => {
-    if (!res.ok) throw (res.text() || res.statusText || "an error has occurred");
-});
+    if (!res.ok) {
+        return res.text().then(msg => { throw msg || res.statusText || "an error has occurred"; });
+    }
+}).catch(handleError);
