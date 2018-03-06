923ac5186bac9d9a36ccb998890b72b7148664ed
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 10:39:29 +0200

    Added options display for confirmation page.

---
+++ app/components/OptionsDisplay.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { formatDate, formatCost } from '../helpers/formatting';
+
+
+export const OptionsDisplay = ({ options, selection }) => {
+
+    const optionkeys = Object.keys(options).filter(x => selection[x]);
+    const total = selection.total;
+
+    return <div>
+        <h2>Registering for:</h2>
+        <ul class="conference-items-display">
+            {optionkeys.map(key => {
+                const option = options[key];
+                const selected = selection[key];
+                const formattedDate = formatDate(option.from, option.to);
+                const formattedCost = formatCost(option.cost);
+                return <li key={key}>
+                    <div className="title">{option.title}</div>
+                    <div className="date">{formattedDate}</div>
+                    <div className="cost"><label>Cost:&nbsp;</label>{formattedCost}</div>
+                </li>;
+            })}
+        </ul>
+        <div className="total">
+            <label>Total Cost:&nbsp;</label>
+            {formatCost(total)}
+        </div>
+    </div>;
+};
