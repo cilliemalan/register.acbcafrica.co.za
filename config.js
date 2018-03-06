14f2e8116902207df10a3878f70f23af6ad5b303
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 21:07:26 +0200

    Added googleAccessToken to config

--- config.js
+++ config.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+const winston = require('winston');
+
+function readJsonOrNull(name) {
+    try {
+        winston.verbose('reading %s as JSON for config', name);
+        const result = fs.readFileSync(name);
+        return JSON.stringify(result);
+    } catch (e) {
+        winston.warn('failed to read %s. The error is %s', name, e);
+        return null;
+    }
+}
+
 const port = process.env.PORT || 3000;
 const root = process.env.URL || "http://localhost:3000/";
 const ghsecret = process.env.GITHUB_HOOK_SECRET;
@@ -15,17 +29,20 @@ const recaptchaSecret = process.env.RECAPTCH_SECRET;
 
 const gaTrackingId = process.env.GA_TRACKING_ID || 'UA-114404288-1';
 
-module.exports = {
-    port,
-    root,
-    ghsecret,
-    loglevel,
-    issuer,
-    audience,
-    clientId,
-    clientSecret,
-    oAuthDomain,
-    recaptchaKey,
-    recaptchaSecret,
-    gaTrackingId
-}
+const googleAccessToken = readJsonOrNull(`${__dirname}/tokens.json`);
+
+    module.exports = {
+        port,
+        root,
+        ghsecret,
+        loglevel,
+        issuer,
+        audience,
+        clientId,
+        clientSecret,
+        oAuthDomain,
+        recaptchaKey,
+        recaptchaSecret,
+        gaTrackingId,
+        googleAccessToken
+    }
