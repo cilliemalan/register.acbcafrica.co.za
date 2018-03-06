2584095bc6fe8b31a289201bf28997a76f324888
Cillié Malan <cilliemalan@gmail.com>
2018-03-06 15:02:26 +0200

    Running recaptcha on API call

--- app/actions/api.js
+++ app/actions/api.js
@@ -1,6 +1,47 @@
 
+
+const recaptchaResolves = [];
+
+window.logRecaptchaReady(() => {
+    window.__onRecaptchaSubmit = (token) => {
+        const numResolves = recaptchaResolves.length;
+        if (numResolves) {
+            recaptchaResolves.forEach((rsv) => {
+                try {
+                    rsv.apply(this, [token]);
+                } catch (e) {
+                    console.error(e);
+                }
+            });
+
+            recaptchaResolves.splice(0, numResolves);
+        }
+    };
+    const _invisibleDiv = document.createElement("div");
+    document.body.appendChild(_invisibleDiv);
+
+    grecaptcha.render(_invisibleDiv, {
+        sitekey: recaptchaKey,
+        callback: '__onRecaptchaSubmit',
+        size: 'invisible'
+    }, true);
+});
+
+function getReCaptchaToken() {
+
+    return new Promise((resolve, reject) => {
+
+        recaptchaResolves.push(resolve);
+        grecaptcha.execute();
+    });
+
+}
+
+
+
+
 function handleError(e) {
-    if(typeof e == "string") {
+    if (typeof e == "string") {
         throw e;
     }
 
@@ -13,7 +54,7 @@ export const fetchForms = () => fetch('/data/forms.json')
         Object.values(forms).forEach(form => {
             form.from = form.from && new Date(form.from);
             form.to = form.to && new Date(form.to);
-        
+
             Object.values(form.options).forEach(option => {
                 option.from = option.from && new Date(option.from);
                 option.to = option.to && new Date(option.to);
@@ -23,14 +64,15 @@ export const fetchForms = () => fetch('/data/forms.json')
         return forms;
     }).catch(handleError);
 
-export const submitRegistration = (registration) => fetch('/api/submit', {
-    method: 'POST',
-    body: JSON.stringify(registration),
-    headers: {
-        'Content-Type': 'application/json'
-    }
-}).then(res => {
-    if (!res.ok) {
-        return res.text().then(msg => { throw msg || res.statusText || "an error has occurred"; });
-    }
-}).catch(handleError);
+export const submitRegistration = (registration) =>
+    getReCaptchaToken().then(token => fetch('/api/submit', {
+        method: 'POST',
+        body: JSON.stringify({ ...registration, token }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })).then(res => {
+        if (!res.ok) {
+            return res.text().then(msg => { throw msg || res.statusText || "an error has occurred"; });
+        }
+    }).catch(handleError);
