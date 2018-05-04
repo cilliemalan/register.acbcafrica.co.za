dfe79c026c7969bd9f5ef2d5f4732bd84fda1e02
Cillié Malan <cilliemalan@gmail.com>
2018-05-04 16:07:37 +0200

    Added polyfill for Element.matches

--- public/polyfills.js
+++ public/polyfills.js
@@ -1,5 +1,20 @@
 'use strict';
 
+if (!Element.prototype.matches) {
+  Element.prototype.matches = 
+      Element.prototype.matchesSelector || 
+      Element.prototype.mozMatchesSelector ||
+      Element.prototype.msMatchesSelector || 
+      Element.prototype.oMatchesSelector || 
+      Element.prototype.webkitMatchesSelector ||
+      function(s) {
+          var matches = (this.document || this.ownerDocument).querySelectorAll(s),
+              i = matches.length;
+          while (--i >= 0 && matches.item(i) !== this) {}
+          return i > -1;            
+      };
+}
+
 //https://tc39.github.io/ecma262/#sec-array.prototype.includes
 if (!Array.prototype.includes) {
     Object.defineProperty(Array.prototype, 'includes', {
