15fcf6ce04bcc707c6d89ac43d0fd5df97c357d3
Cillié Malan <cilliemalan@gmail.com>
2018-05-04 16:07:54 +0200

    Busy adding suboptions

--- app/components/OptionsEditor.jsx
+++ app/components/OptionsEditor.jsx
@@ -12,6 +12,9 @@ export class OptionsEditor extends React.Component {
 
         this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
         this.itemSelectedChanged = this.itemSelectedChanged.bind(this);
+        this.recalculateTotal = this.recalculateTotal.bind(this);
+        this.suboptionSelectedChanged = this.suboptionSelectedChanged.bind(this);
+        this._fireOnChange = this._fireOnChange.bind(this);
     }
 
     componentWillReceiveProps(props) {
@@ -34,20 +37,31 @@ export class OptionsEditor extends React.Component {
         }
     }
 
-    itemSelectedChanged(e) {
-        const option = e.target.name.match(/^chk-(.+)$/)[1];
-        const checked = e.target.checked;
+    recalculateTotal(options) {
+        return Object.keys(options)
+            .filter(option => options[option])
+            .reduce((acc, option) =>
+                (acc || 0) + (options[option]
+                    ? (isFinite(this.props.options[option].cost) ? this.props.options[option].cost : 0)
+                    : 0),
+                undefined);
+    }
 
-        const recalculateTotal = (options) =>
-            Object.keys(options)
-                .filter(option => options[option])
-                .reduce((acc, option) =>
-                    (acc || 0) + (options[option]
-                        ? (isFinite(this.props.options[option].cost) ? this.props.options[option].cost : 0)
-                        : 0),
-                    undefined);
+    _fireOnChange() {
+        this.props.input.onChange({ ...this.state.value, total: this.state.total })
+    }
+
+    itemSelectedChanged(e) {
+        const checkbox = e.target;
+        const option = checkbox.name.match(/^chk-(.+)$/)[1];
+
 
         if (option) {
+            const suboptions = this.props.options[option].options;
+            const firstsubopt = suboptions && Object.keys(suboptions)[0];
+            const checked = checkbox.checked
+                ? (suboptions ? firstsubopt : true)
+                : false;
 
             this.setState((state) => {
                 const value = {
@@ -57,18 +71,42 @@ export class OptionsEditor extends React.Component {
                 return {
                     ...state,
                     value,
-                    total: recalculateTotal(value)
+                    total: this.recalculateTotal(value)
                 };
-            }, () => this.props.input.onChange({ ...this.state.value, total: this.state.total }));
+            }, this._fireOnChange);
 
+        }
+    }
+
+    suboptionSelectedChanged(e) {
+        const radio = e.target;
+        const [_, option, suboption] = radio.id.match(/^so-(.+)-(.+)$/);
+        const checkedvalue = option && radio.checked && suboption;
+
+        if (checkedvalue) {
+            if (this.state.value[option] != checkedvalue) {
+                this.setState((state) => {
+                    const value = {
+                        ...state.value,
+                        [option]: checkedvalue
+                    };
+                    return {
+                        ...state,
+                        value,
+                        total: this.recalculateTotal(value)
+                    };
+                }, this._fireOnChange);
+            }
         }
     }
 
     clickedItem(e) {
         if (e.target.type != 'checkbox' && !/\bcss-label\b/.test(e.target.className)) {
-            const checkbox = e.currentTarget.querySelector('[type=checkbox]');
-            checkbox.focus();
-            checkbox.click();
+            if (!e.target.matches('.details > ul.suboptions,.details > ul.suboptions *')) {
+                const checkbox = e.currentTarget.querySelector('[type=checkbox]');
+                checkbox.focus();
+                checkbox.click();
+            }
         }
     }
 
@@ -80,27 +118,49 @@ export class OptionsEditor extends React.Component {
         const { error, touched } = this.props.meta;
         const { onFocus, onBlur } = this.props.input;
 
-        return <div>
+        return <div ref={this._ref}>
             <h2>Conference Options</h2>
             <p>Please select all that apply.</p>
             <ul className='conference-items'>
                 {optionkeys.map(key => {
                     const option = poptions[key];
-                    const selected = soptions[key];
+                    const selectedvalue = soptions[key];
+                    const suboptions = option.options;
+                    const suboptionkeys = suboptions && Object.keys(suboptions);
                     const formattedDate = formatDate(option.from, option.to);
                     const formattedCost = formatCost(option.cost);
+
+                    const render_suboptions = () => <ul className="suboptions">
+                        {suboptionkeys.map(suboptionkey => {
+                            const suboption = suboptions[suboptionkey];
+                            const i_name = `so-${key}`;
+                            const i_id = `so-${key}-${suboptionkey}`;
+                            const formattedCost = formatCost(suboption.cost);
+
+                            return <li key={suboptionkey}>
+                                <input type="radio"
+                                    name={i_name}
+                                    checked={suboptionkey == selectedvalue}
+                                    onChange={this.suboptionSelectedChanged}
+                                    id={i_id} />
+                                <label htmlFor={i_id} className="subopt-title">{suboption.title} ({formattedCost})</label>
+                            </li>;
+                        })}
+                    </ul>;
+
                     return <li key={key} onClick={this.clickedItem}>
                         <div className="details">
                             <div className="title">{option.title}</div>
                             <div className="date">{formattedDate}</div>
-                            {formattedCost !== '' ? (<div className="cost"><label>Cost:&nbsp;</label>{formattedCost}</div>) : undefined}
+                            {selectedvalue && suboptionkeys ? render_suboptions() : undefined}
+                            <div className="cost"><label>Cost:&nbsp;</label>{formattedCost}</div>
                         </div>
                         <div className="selected">
                             <input type="checkbox"
                                 className='css-checkbox'
                                 name={`chk-${key}`}
                                 id={`chk-${key}`}
-                                checked={selected}
+                                checked={selectedvalue}
                                 onFocus={onFocus}
                                 onBlur={() => onBlur()}
                                 onChange={this.itemSelectedChanged} />
