01e03f0fbbd5503c5fb1c2cad69aa4a2f1e2ca1e
Cillié Malan <cilliemalan@gmail.com>
2018-06-19 13:50:25 +0200

    Added summary display for childcare info

--- app/components/SubmissionDetails.jsx
+++ app/components/SubmissionDetails.jsx
@@ -1,8 +1,57 @@
 import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
-export const SubmissionDetails = ({ submission, forms }) => {
-    const { details: { title, firstname, lastname, contactNumber, email, country, church } } = submission;
+const ChildcareDetails = ({ children, childcare }) => {
+
+    const days = childcare && childcare.days || [];
+    const slots = _(days)
+        .flatMap(day => day.slots)
+        .uniq()
+        .orderBy(x => x)
+        .value();
+    const dates = days.map(({ date }) => date);
+    const dateDays = days.map(({ date }) => moment(date).format('ddd'));
+    const daysByDate = {};
+    days.forEach(day => daysByDate[day.date] = day.slots);
+
+    const slotsDisplay = (selectedDays) => {
+
+        return selectedDays.map((selectedSlots, d_ix) => {
+            const dayDetails = dateDays[d_ix];
+            const slotDisplay = selectedSlots
+                .map((yes, s_ix) => yes && slots[s_ix])
+                .filter(x => x)
+                .map((text, ix) => <Fragment>
+                    {ix != 0 && " and "}
+                    <strong>{text}</strong>
+                </Fragment>)
+            return slotDisplay.length && <Fragment>
+                <strong>{dayDetails}</strong>&nbsp;
+                {slotDisplay}
+            </Fragment>;
+        })
+        .filter(x => x)
+        .map((stuff, ix) => <Fragment>{ix != 0 && ", "}{stuff}</Fragment>)
+    }
+    return <Fragment>
+        Children: <br />
+        {children.map(({ name, age, days }) =>
+            <div className="child-summary">
+                <strong>{name}</strong> (<strong>{age}</strong> years old) for {slotsDisplay(days)}<br />
+            </div>)}
+    </Fragment>
+}
+
+export const SubmissionDetails = ({ submission, form }) => {
+    const { details: { title,
+        firstname, lastname, contactNumber,
+        email, country, church,
+        childcare, children } } = submission;
+
+    const hasChildcare = !!form.childcare;
+
+
     return <div>
         <h2>Personal Information</h2>
         <p>
@@ -11,6 +60,8 @@ export const SubmissionDetails = ({ submission, forms }) => {
             Email: <strong>{email}</strong><br />
             Country: <strong>{country}</strong><br />
             {church && <Fragment>Church: <strong>{church}</strong><br /></Fragment>}
+            {hasChildcare && <Fragment>Childcare: <strong>{childcare ? "Yes" : "No"}</strong><br /></Fragment>}
+            {hasChildcare && childcare && <ChildcareDetails children={children} childcare={form.childcare} />}
         </p>
     </div>;
 }
