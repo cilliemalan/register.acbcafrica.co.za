5a5d78a105c556cc1c2cdf92d2729051f1c505ff
Cillié Malan <cilliemalan@gmail.com>
2021-01-14 17:46:02 +0200

    Updated sendgrid for dynamic templates

--- api/sendgrid.js
+++ api/sendgrid.js
@@ -4,7 +4,6 @@ const { sendGridKey, defaultFromEmail } = require('../config');
 
 if (sendGridKey) {
     sgMail.setApiKey(sendGridKey);
-    sgMail.setSubstitutionWrappers('{{', '}}');
     winston.info('sendgrid API configured');
 } else {
     winston.info('not using sendgrid because no API key was specified');
@@ -15,7 +14,7 @@ module.exports = {
         if (sendGridKey) {
             winston.info(`sending transactional mail to ${to} from ${from} subject ${subject || '(default)'} using template ${templateId}`);
             winston.silly(`substiutions: %O`, substitutions);
-            var promise = sgMail.send({ to, from, subject, templateId, substitutions });
+            var promise = sgMail.send({ to, from, subject, templateId, dynamicTemplateData: substitutions });
             promise.then(() => winston.verbose(`transactional mail successfully sent to ${to} using template ${templateId}`))
                 .catch(e => winston.error(`failed to send transactional mail to ${to} using template ${templateId}: ${e}`));
             return promise;
