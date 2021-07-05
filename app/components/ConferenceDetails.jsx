fad23d8e3a69c9730117dfab2313be23aeeb7d7b
Cillié Malan <cilliemalan@gmail.com>
2021-07-05 11:37:52 +0200

    Fixed location display when no location

--- app/components/ConferenceDetails.jsx
+++ app/components/ConferenceDetails.jsx
@@ -7,16 +7,22 @@ export const ConferenceDetails = ({ form, message }) => {
     const { from, to, location, address, title } = form;
     const dateDisplay = formatDateSentence(form.from, form.to);
 
-    return <div>
-        <p>
-            You are registering for {" "}
-            <strong>{title}</strong>.
-        </p>
+    const registeringForLine = <p>
+        You are registering for {" "}
+        <strong>{title}</strong>.
+    </p>;
+
+    const locationLine = location ?
         <p>The conference will be held at {" "}
             <strong>{location}</strong>: {" "}
             ({address} <a href={`https://www.google.co.za/maps/search/${encodeURIComponent(address)}?hl=en&source=opensearch`} target="blank">see map</a>) {" "}
             <strong>{dateDisplay}</strong>.
         </p>
+        : undefined;
+
+    return <div>
+        {registeringForLine}
+        {locationLine}
         <p>{message}</p>
     </div>;
 }
